df9318a4d6ab204e9095fa3e14e8a3d123db51e4
dragana <dragana_zivkovic@rocketmail.com>
2019-10-13 20:29:42 +0200

    new todo

--- src/App.js
+++ src/App.js
@@ -1,74 +1,107 @@
-import React from 'react';
-import TodoList from './TodoList'
-import TodoItems from './TodoItems'
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends React.Component {
-	constructor() {
-		super()
+class App extends Component {
 
-		this.state= {
-			items: [],
-			currentItem: {
-				text:'',
-				key:''
-			},
-		}
+  constructor(props){
+    super(props);
+    this.state={
+      title: 'React Simple CRUD Todo Application',
+      act: 0,
+      index: '',
+      datas: []
+    }
+  } 
 
-	}
+  componentDidMount(){
+    this.refs.task.focus();
+  }
 
-	handleInput = e => {
-		const itemText = e.target.value
-		const currentItem = {text: itemText, key: Date.now()}
-		this.setState({
-			currentItem,
-		})
-	}
+  fSubmit = (e) =>{
+    e.preventDefault();
 
-	addItem = e => {
-		//prevent reloading from submitting the form
-		e.preventDefault()
-		const newItem = this.state.currentItem
-		if (newItem.text !== '') {
-			const items= [...this.state.items, newItem]
-			this.setState({
-				items: items,
-				currentItem: {text: '', key: ''},
-			})
-		}
-	}
+    let datas = this.state.datas;
+    let task = this.refs.task.value;
 
-	deleteItem = key => {
-		const filteredItems = this.state.items.filter(item => {
-			return item.key !== key
-		})
-		this.setState({
-			items: filteredItems,
-		})
-	}
 
- 
+    if(task) { 
+        if(this.state.act === 0){   //new
+        let data = {
+        task
+      }
+      datas.push(data);
+      }else{                      //update
+        let index = this.state.index;
+        datas[index].task = task;
+
+      }    
+
+      this.setState({
+        datas: datas,
+        act: 0
+      });
+
+      this.refs.myForm.reset();
+      this.refs.task.focus();
+           
+    }
+    
+  }
+
+  fRemove = (i) => {
+    let datas = this.state.datas;
+    datas.splice(i,1);
+    this.setState({
+      datas: datas
+    });
+
+    this.refs.myForm.reset();
+    this.refs.task.focus();
+  }
+
+  fEdit = (i) => {
+    let data = this.state.datas[i];
+    this.refs.task.value = data.task;
+   
+
+    this.setState({
+      act: 1,
+      index: i
+    });
+
+    this.refs.task.focus();
+  }  
+
 
   render() {
-  	return (
-  		<div className="App">
-  			<h2 className="title">Todo app</h2>
-  			<p className="title-text">Enter the task in the input box, click on add button to add to the list. 
-  			To remove from the list, click on the button trash can to be removed.</p>
-  			<TodoList 
-  				addItem={this.addItem}
-  				inputElement={this.inputElement}
-  				handleInput={this.handleInput}
-  				currentItem={this.state.currentItem}
-  			/>
-  			<TodoItems 
-  				entries={this.state.items} 
-  				deleteItem={this.deleteItem}
-  			/>
-  		</div>
-  		)
+    let datas = this.state.datas;
+    return (
+      <div className="App">
+        <h2>{this.state.title}</h2>
+        <br/>
+        <h6>Enter the task in the input box, click on add button to add to the list. If task exists, you can remove it (trash can button) or edit existing entry (pen button).</h6>
+        <form ref="myForm" className="myForm">
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">Task</label>
+            <input type="text" ref="task" placeholder="your task" className="formField form-control col-sm-9"/>
+          </div>
+          <button onClick={(e)=>this.fSubmit(e)} className="myButton btn btn-success ml-2 btn-lg">ADD </button>
+        </form>
+        <div className="todo">
+          {datas.map((data, i) =>
+            <li key={i} className="myList">
+              {i+1}. {data.task}
+              <div className="buttonsStyle">
+              <button onClick={()=>this.fRemove(i)} className="myListButton btn btn-outline-success ml-2"><i className="fas fa-trash-alt"></i> </button>
+              <button onClick={()=>this.fEdit(i)} className="myListButton btn btn-outline-success ml-2"><i className="fas fa-pencil-alt"></i> </button>
+              </div>
+            </li>
+          )}
+        </div>
+       
+      </div>
+    );
   }
-  	
 }
 
 export default App;
